9e55e15ee493b399900c3027aa633c0ac7edb5b6
sabermaraghi <s.maraghie@gmail.com>
2025-11-09 20:59:30 +0330

    k8s-pod-cleaner issue fixed

--- scripts/advanced/day5/k8s-pod-cleaner.sh
+++ scripts/advanced/day5/k8s-pod-cleaner.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 set -euo pipefail
 IFS=$'\n\t'
-
 LOG="/var/log/k8s-cleaner.log"
 NAMESPACES=("default" "kube-system" "monitoring")
-
 log() { echo "[$(date +'%Y-%m-%d %H:%M:%S')] [K8S] $*" | tee -a "$LOG"; }
-
 clean_namespace() {
   local ns="$1"
   local pods=$(kubectl get pods -n "$ns" --field-selector=status.phase=Failed -o json | jq -r '.items[].metadata.name')
   [[ -z "$pods" ]] && return
-
   for pod in $pods; do
-    log "INFO" Delete the currupted Pod: $ns/$pod"
+    log "INFO" "Delete the corrupted Pod: $ns/$pod"
     kubectl delete pod "$pod" -n "$ns" --grace-period=0 --force &
   done
 }
-
-log "INFO" "Start cleaning broaken Pods."
+log "INFO" "Start cleaning broken Pods."
 for ns in "${NAMESPACES[@]}"; do
   clean_namespace "$ns"
 done
 wait
 log "SUCCESS" "Cleaning is finished."
+
